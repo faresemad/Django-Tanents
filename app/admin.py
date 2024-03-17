9e89f1a9ef59b877839415995cdc02baaad5a531
Fares Emad <faresemadx@gmail.com>
2024-03-18 00:57:08 +0200

    Add TenantAdminMixin to ClientAdmin and create DomainAdmin class

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django_tenants.admin import TenantAdminMixin
 
-# Register your models here.
+from app.models import Client, Domain
+
+
+@admin.register(Client)
+class ClientAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ["name", "created_on"]
+
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ["domain", "tenant"]
